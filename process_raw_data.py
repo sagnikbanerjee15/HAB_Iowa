b4ffdf61c3722afea69b54128a911986b72296cb
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-05-30 20:06:37 -0500

    Update process_raw_data.py

--- process_raw_data.py
+++ process_raw_data.py
@@ -1,14 +1,17 @@
 
 import glob
 import pandas as pd
+import os
 
 def readRawDataFiles():
-    all_files = glob.glob("raw_data/*")
-    iowa_data_df = pd.DataFrame()
-    for file_num,eachfile in enumerate(all_files): 
-        iowa_data_df = iowa_data_df.append( pd.read_csv(eachfile,low_memory=False))
-        print(f"Finished appending {file_num+1} files")
-    print(iowa_data_df.head(5))
+    if os.path.exists("raw_data/iowa.csv")==False:
+        all_files = glob.glob("raw_data/*")
+        iowa_data_df = pd.DataFrame()
+        for file_num,eachfile in enumerate(all_files): 
+            iowa_data_df = iowa_data_df.append( pd.read_csv(eachfile,low_memory=False))
+            print(f"Finished appending {file_num+1} files")
+        print(iowa_data_df.head(5))
+        iowa_data_df.to_csv("raw_data/iowa.csv")
 
 def main():
     # Read in the csv files from raw data directory
